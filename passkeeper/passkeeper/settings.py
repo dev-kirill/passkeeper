9675fd31999d47e77d9f8cbe2bb6be366e12b0c5
dev_fedorovlab <dev@fedorovlab.ru>
2017-09-27 21:32:51 +0400

    Commit1

--- passkeeper/passkeeper/settings.py
+++ passkeeper/passkeeper/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '.slyuznev.ru',
+    '127.0.0.1',
     ]
 
 
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
